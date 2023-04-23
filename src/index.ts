ca558ed90f6580b65f922255b1f49c95e793c128
savifon <savifon1@gmail.com>
2023-04-23 12:40:41 -0300

    headers settings

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,12 @@ mongoose.connect('mongodb://localhost:27017')
     const app = express();
     const port = 3003;
 
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+      next();
+    });
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
